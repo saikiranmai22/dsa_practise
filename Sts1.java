a8114e3318b6764bbcf99b90aa378fb6f2c17aa8
saikiranmai22 <saikiranmai.n11@gmail.com>
2024-08-08 18:56:49 +0530

    first code

---
+++ Sts1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class Sts1{
+static int findMinX(int num[],int rem[], int k)
+    {
+        int x=1;
+        while(true){
+            int j;
+            for(j=0;j<k;j++){
+                if(x%num[j]!=rem[j]){
+                break;
+            }
+            }
+            if(j==k){
+                return x;
+            }
+            x++;
+        }
+    }
+public static void main(String args[]){
+    int num[]={5,7};
+    int rem[]={1,3};
+    int k=num.length;
+    System.out.println(findMinX(num,rem,k));
+}}
