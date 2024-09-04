66d567db0444665775705d5d77509ef6af00a9fc
saikiranmai22 <saikiranmai.n11@gmail.com>
2024-09-04 17:24:40 +0530

    new changes

---
+++ longestSeqOf1.java
@@ -0,0 +1,26 @@
+public class longestSeqOf1 {
+    static int func(int arr[], int k){
+        int start=0;
+        int end=0;
+        int count=0;
+        int max=-1;
+        for(end=0;end<arr.length;end++){
+            if(arr[end]==0){
+                count++;
+            }
+            while(count>k){
+                if(arr[start]==0){
+                    count--;
+                }
+                start++;
+            }
+            max=Math.max(max,end-start+1);
+        }        
+        return max;
+    }
+    public static void main(String args[]){
+        int arr[]={1,1,1,0,0,0,1,1,1,1};
+        int k=2;
+        System.out.println(func(arr,k));
+    }
+}
